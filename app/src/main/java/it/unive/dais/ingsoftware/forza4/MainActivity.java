bc3f2ec42a3ded3b2f8e41bb3800ed2f3459749b
dmoruz97 <davide.moruzzi97@hotmail.it>
2018-11-14 20:52:08 +0100

    Changed layout and creation of new Activity

--- app/src/main/java/it/unive/dais/ingsoftware/forza4/MainActivity.java
+++ app/src/main/java/it/unive/dais/ingsoftware/forza4/MainActivity.java
@@ -1,7 +1,10 @@
 package it.unive.dais.ingsoftware.forza4;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import java.io.IOException;
 import java.util.concurrent.ExecutionException;
@@ -12,7 +15,6 @@ import it.unive.dais.legodroid.lib.comm.BluetoothConnection;
 import it.unive.dais.legodroid.lib.comm.Channel;
 import it.unive.dais.legodroid.lib.comm.SpooledAsyncChannel;
 import it.unive.dais.legodroid.lib.sensors.TouchSensor;
-import it.unive.dais.legodroid.lib.util.Consumer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +22,46 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Recupero bottone per NUOVA PARTITA
+        Button newGameButton = findViewById(R.id.newGameButton);
+        newGameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent openNewGameActivity = new Intent(MainActivity.this, NewGameActivity.class);
+                startActivity(openNewGameActivity);
+            }
+        });
+
+        // Recupero bottone per CARICA PARTITA
+        Button loadGameButton = findViewById(R.id.loadGameButton);
+        loadGameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent openLoadGameActivity = new Intent(MainActivity.this, LoadGameActivity.class);
+                startActivity(openLoadGameActivity);
+            }
+        });
+
+        // Recupero bottone per STATISTICHE
+        Button statisticsButton = findViewById(R.id.statisticsButton);
+        statisticsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent openStatisticsActivity = new Intent(MainActivity.this, StatisticsActivity.class);
+                startActivity(openStatisticsActivity);
+            }
+        });
+
+        // Recupero bottone per OPZIONI
+        Button optionsButton = findViewById(R.id.optionsButton);
+        optionsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent openOptionsActivity = new Intent(MainActivity.this, OptionsActivity.class);
+                startActivity(openOptionsActivity);
+            }
+        });
         
         BluetoothConnection conn = new BluetoothConnection("EV3");
         Channel channel = null;
@@ -29,6 +71,7 @@ public class MainActivity extends AppCompatActivity {
         catch (IOException e) {
             e.printStackTrace();
         }
+
         EV3 ev3 = new EV3(new SpooledAsyncChannel(channel));
 
         ev3.run(data -> {
---
+++ app/src/main/java/it/unive/dais/ingsoftware/forza4/NewGameActivity.java
@@ -0,0 +1,15 @@
+package it.unive.dais.ingsoftware.forza4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import it.dais.forza4.R;
+
+public class NewGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_game);
+    }
+}
