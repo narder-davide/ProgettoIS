00543ff0f93fb5e8033ee4162de4db28fe6b3256
dmoruz97 <davide.moruzzi97@hotmail.it>
2018-11-21 11:57:26 +0100

    NewGame changed

--- app/src/main/java/it/unive/dais/ingsoftware/forza4/NewGameActivity.java
+++ app/src/main/java/it/unive/dais/ingsoftware/forza4/NewGameActivity.java
@@ -1,15 +1,26 @@
 package it.unive.dais.ingsoftware.forza4;
 
+import android.os.Handler;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Calendar;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import it.dais.forza4.R;
 
 public class NewGameActivity extends AppCompatActivity {
 
+    TextView timerValue = findViewById(R.id.timerValue);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_game);
+
+
     }
 }
